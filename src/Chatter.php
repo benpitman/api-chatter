0d9d7caaa2392209c34f060bb4312cc5dcf2b078
Ben Pitman <pitman.ben@gmail.com>
2023-08-25 12:54:06 +0100

    Initial commit

---
+++ src/Chatter.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App;
+
+use App\Core\Store\Config;
+use App\Core\Store\Local;
+use Kentron\AApp;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+use App\Core\Store\Variable\Variable;
+
+use App\Http\Router;
+use App\Module\Core\System\Variable\VariableSqlService;
+use Symfony\Component\Console\Helper\Dumper;
+use Symfony\Component\VarDumper\Dumper\CliDumper;
+
+use function rr\dd;
+
+/**
+ * The inital application class, injected into the controllers
+ */
+final class Chatter extends AApp
+{
+    public function resetStores(): void
+    {
+        // Config::reset();
+        // Variable::reset();
+        // Local::reset();
+    }
+
+    public function boot (): void
+    {
+        dd("hello");
+        // $this->reset();
+        // $this->bootRouter();
+        // $this->loadConfig();
+        // $this->bootOrm();
+        // $this->loadVariables();
+    }
+
+    protected function loadConfig (): void
+    {
+        // Config::load();
+    }
+
+    protected function loadVariables (): void
+    {
+        // Variable::build(VariableSqlService::getAll(), Config::getDatabaseKey());
+    }
+
+    // protected function allowedAccess (): bool
+    // {
+    //     return Variable::getSystemAccess();
+    // }
+
+    protected function bootRouter (): void
+    {
+        // Router::loadAllRoutes($this);
+    }
+
+    protected function bootOrm (): void
+    {
+        // $capsule = new Capsule();
+
+        // $capsule->addConnection(Config::getDatabaseConfig());
+        // $capsule->setAsGlobal();
+        // $capsule->bootEloquent();
+    }
+}
